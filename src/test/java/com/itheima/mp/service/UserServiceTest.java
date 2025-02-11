2de70ac24adaea639dc95e1219f4b2705e7195cf
Wang Xilai <1257811052@qq.com>
2025-02-11 11:41:38 +0800

    IService saveBatch test commit

--- src/test/java/com/itheima/mp/service/UserServiceTest.java
+++ src/test/java/com/itheima/mp/service/UserServiceTest.java
@@ -43,4 +43,38 @@ class UserServiceTest {
     void testQueryByIds() {
         userService.listByIds(List.of(1L, 2L, 3L, 4L)).forEach(System.out::println);
     }
+
+    // 测试批量新增
+    private User createUser( int i) {
+        User user = new User();
+        user.setUsername("User" + i);
+        user.setPassword("123");
+        user.setPhone("18688990019" + i);
+        user.setBalance(2000);
+        user.setInfo("{\"age\": 24, \"intro\": \"英文老师\",\"gender\": \"female\"}");
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateTime(LocalDateTime.now());
+        return user;
+    }
+
+    @Test
+    void testSaveBatch() {
+        // 先用for循环保存1000条数据
+        // 开始时间
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            userService.save(createUser(i));
+        }
+        // 结束时间
+        long end = System.currentTimeMillis();
+        System.out.println("for循环保存1000条数据耗时：" + (end - start) + "ms");
+
+        // 再用saveBatch保存1000条数据
+        // 开始时间
+        start = System.currentTimeMillis();
+        userService.saveBatch(List.of(createUser(1001), createUser(1002), createUser(1003)));
+        // 结束时间
+        end = System.currentTimeMillis();
+        System.out.println("saveBatch保存1000条数据耗时：" + (end - start) + "ms");
+    }
 }
