768835ac1fd13175bd5468b662a6b813c20aff46
Wang Xilai <1257811052@qq.com>
2025-02-24 21:40:50 +0800

    MyBatisPlus插件代码生成测试commit

---
+++ src/main/java/com/itheima/mp/mapper/AddressMapper.java
@@ -0,0 +1,16 @@
+package com.itheima.mp.mapper;
+
+import com.itheima.mp.domain.dto.Address;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wxl
+ * @since 2025-02-24
+ */
+public interface AddressMapper extends BaseMapper<Address> {
+
+}
